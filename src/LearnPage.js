bc9913991a8d43ae8e87f7409e0accc71fbdbd37
do_hh <do_hh@naver.com>
2024-03-31 20:52:55 +0900

    commit

--- src/LearnPage.js
+++ src/LearnPage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import TestComponent from './TestComponent';
 
 function LearnPage() {
   return (
-    <div>
-      <p>이곳은 React 과제를 구현하는 페이지입니다.</p>
-      <p>아래에 과제로 드린 컴포넌트를 만들어 보세용</p>
+    <div style={{textAlign:'center'}}>
+      <h2>
+        이곳은 React 과제를 구현하는 페이지 입니다
+        <br />
+        아래에 과제로 그린 컴포넌트를 만들어 보세용
+      </h2>
+      <h3>참고자료</h3>
+      <br />
+      <a href='https:ko.reactjs.org/docs/components-and-props.html'>React 공식 문서</a>
+      <br />
+      <a href='https://goddaehee.tistory.com/299'>React 컴포넌트란?</a>
+    
+      <TestComponent />
+
     </div>
   );
 }
---
+++ src/TestComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TestComponent(){
+    return <div>컴포넌트 만들었습니다.</div>
+}
+
+export default TestComponent;
