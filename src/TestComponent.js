ccf18fcf94a4bfbb1e3a8ba69e7a489a80a7c8e1
kimhyungjun <gim54676@gmail.com>
2024-04-01 23:06:19 +0900

    김형준입니다.

---
+++ src/TestComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TestComponent {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <main>
+                <h3></h3>
+                    컴포넌트 만들었습니다.
+                </main>
+            </div>
+        );
+    }
+}
+
+export default TestComponent; //다른 JS파일에서 불러올 수 있도록 내보내주기
